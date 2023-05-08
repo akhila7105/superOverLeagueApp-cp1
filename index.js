f1c7a0f42867236218020ecb2500d357d0e8c82e
akhila7105 <akhilakoripelli21@gmail.com>
2023-05-08 05:26:12 +0000

    superOverLeagueApp-cp1

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="superOver ">
+    <h1 className="heading">Super Over League</h1>
+    <div className="card">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
